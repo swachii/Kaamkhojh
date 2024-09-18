c2ceb1e88f0258097c8fa6531ab26219c5255b5c
swachii <be2019se692@gces.edu.np>
2024-09-18 21:57:48 +0545

    Feat: Add clear input functionality to Search component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className='navBar flex justify-between items-center p-{3rem}'>
-     <div className="logoDiv">
+    <div className='navBar flex justify-between items-center p-12'>
+      <div className="logoDiv">
         <h1 className="logo text-[25px] text-blueColor">
-            <strong>Kaam</strong>Khojh</h1>
-     </div>
-
+          <strong>Kaam</strong>Khojh
+        </h1>
+      </div>
+      <nav className='menu flex gap-8'>
+        <ul className="flex space-x-8">
+          <li className="menuList text-[#6f6f6f] hover:text-blueColor">Jobs</li>
+          <li className="menuList text-[#6f6f6f] hover:text-blueColor">Companies</li>
+          <li className="menuList text-[#6f6f6f] hover:text-blueColor">About</li>
+          <li className="menuList text-[#6f6f6f] hover:text-blueColor">Contact</li>
+          <li className="menuList text-[#6f6f6f] hover:text-blueColor">Blog</li>
+          <li className="menuList text-[#6f6f6f] hover:text-blueColor">Login</li>
+          <li className="menuList text-[#6f6f6f] hover:text-blueColor">Register</li>
+         </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/Components/SearchDiv/Search.jsx
+++ src/Components/SearchDiv/Search.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { AiOutlineCloseCircle, AiOutlineSearch } from 'react-icons/ai'; // Import the search icon
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className='searchDiv grid gap-10 bg-greyIsh rounded-[10px] p-[3rem]'>
+      <form action="">
 
-export default Search
+        <div className='firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg'>
+          
+          <div className="flex gap-2 items-center w-full">
+            <AiOutlineSearch className='text-[25px] icon'/>
+            <input  type="text" className='bg-transparent text-blue-500 focus:outline-none w-[!00%]' 
+              placeholder='Search Job Here.....'/>
+            <AiOutlineCloseCircle className='text-[30px] text-[#a5a6a6] hover:text-textColor icon'/>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
+export default Search;
