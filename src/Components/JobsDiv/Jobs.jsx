471e0e783d1a45a6b589ea9f4dc0c9245e18e8dc
swachii <be2019se692@gces.edu.np>
2024-09-21 20:44:28 +0545

    minor changes in UI

--- src/Components/JobsDiv/Jobs.jsx
+++ src/Components/JobsDiv/Jobs.jsx
@@ -1,59 +1,46 @@
 import React from 'react';
 import { BiTimeFive } from 'react-icons/bi';
+import logo1 from '../../Assets/logo1.png'; // Importing the logo
 
-const jobData = [
-  {
-    id: 1,
-    title: 'Web Developer',
-    location: 'Canada',
-    time: 'Now',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus impedit rerum ducimus, illum sequi mollitia.',
-  },
-  {
-    id: 2,
-    title: 'Software Engineer',
-    location: 'USA',
-    time: '2 days ago',
-    description: 'Temporibus odio labore odit mollitia quaerat magni qui repellat, est ad voluptatem eveniet laudantium autem.',
-  },
-  {
-    id: 3,
-    title: 'UI/UX Designer',
-    location: 'Remote',
-    time: '1 week ago',
-    description: 'Voluptatibus impedit rerum ducimus, illum sequi mollitia. Temporibus odio labore odit mollitia quaerat magni qui repellat.',
-  },
-  {
-    id: 4,
-    title: 'Data Scientist',
-    location: 'UK',
-    time: '5 hours ago',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat magni qui repellat, est ad voluptatem.',
-  },
-];
+// Data for the job listing
+const Data = {
+  id: 1,
+  image: logo1,
+  title: 'Web Developer',
+  time: 'Now',
+  location: 'Canada',
+  desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus impedit rerum ducimus, illum sequi mollitia. Temporibus odio labore odit mollitia quaerat magni qui repellat, est ad voluptatem eveniet laudantium autem!',
+  company: 'Novac Linus Co.'
+};
 
 const Jobs = () => {
   return (
-    <div className="jobContainer flex gap-10 justify-center flex-wrap items-center py-10">
-      {jobData.map((job) => (
-        <div
-          key={job.id}
-          className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg transition-all duration-300"
-        >
+    <div>
+      <div className="jobContainer flex gap-10 justify-center flex-wrap items-center py-10">
+        <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
           <div className="flex justify-between items-center gap-4">
             <h1 className="text-[16px] font-semibold text-textColor group-hover:text-white">
-              {job.title}
+              {Data.title}
             </h1>
             <span className="flex items-center text-[#ccc] gap-1">
-              <BiTimeFive /> {job.time}
+              <BiTimeFive /> {Data.time}
             </span>
           </div>
-          <h6 className="text-[#ccc]">{job.location}</h6>
+          <h6 className="text-[#ccc]">{Data.location}</h6>
           <p className="text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-white">
-            {job.description}
+            {Data.desc}
           </p>
+
+          <div className="company flex items-center gap-2">
+            <img src={Data.image} alt="Company Logo" className="w-[10%]" />
+            <span className="text-[14px] py-[1rem] block group-hover:text-white">{Data.company}</span>
+          </div>
+          
+          <button className='border-[2px] rounded-[10px] block p-[10px] w-full text-[14px] font-semibold text-textColor bg-white group-hover/item:text-textColor group-hover:text-white'>
+            Apply Now
+          </button>
         </div>
-      ))}
+      </div>
     </div>
   );
 };
